8a9791dd0702ca6c085eb5173147e74367220f15
Rasmus Kilp <RKilp@spartaglobal.com>
2019-12-02 12:13:41 +0000

    reviewed print and type and variables

---
+++ README.md
@@ -0,0 +1,14 @@
+#Pythob basics eng 48::
+
+this repo is the basics of pyhon and stuff
+
+## Git basics
+
+things we did :
+-added git
+-added gitignore
+-git commit -m 'a message'
+- git status
+ - to check status of current files i.e whether it is added, (un)commited
+ things in python
+ - how to run file
---
+++ python_basics_1.py
@@ -0,0 +1,15 @@
+#variables, the print statement
+## it's like a box
+## it has a name and has stuff inside
+## you can re-use the box
+##variable assignment
+les_variable = 'good afternoon eng_48'
+##calling a variable
+print(les_variable)
+## Re_assign what is inside the box
+example_variable2 = 48
+print(example_variable2)
+# print statement -- > outputs to console
+# Print with type ()
+print(type(les_variable))
+print(type(example_variable2))
