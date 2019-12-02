56792ef4c055a4b5db4530964aed9199b555f96d
Rasmus Kilp <RKilp@spartaglobal.com>
2019-12-02 17:05:09 +0000

    finished lists and dicts

---
+++ python_basics_105_dicts.py
@@ -0,0 +1,12 @@
+#Dictionary
+#it works like a REAL dictionary
+#it has key and value pairs
+#the key is the word you look up
+#the value is the associated item to the key
+#syntax
+#dict = {'key' : 'value'}
+#defining a dict
+my_dictionary = {'tissues': 'an item made out of some mixture','baseball_bat':'an object to hit balls with'}
+#to access dicts you use SQUARE brackets
+my_dictionary['baseball_bat'] = 'placeholder description'
+print(my_dictionary['baseball_bat'])
